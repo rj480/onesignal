671b76d176bfa18b60f672aa5aa78c780b51a0a6
DESKTOP-MRS4S5L\sobedone <rjam480@gmail.com>
2017-05-02 17:58:26 -0500

    inicio del proyecto

---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,23 @@
+// Initialize app
+var myApp = new Framework7({
+    material:true,
+    swipePanel:'both',
+   
+});
+
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
